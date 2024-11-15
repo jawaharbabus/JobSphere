235f4449120a601446bf79b6cb378dc6c94cabc4
Parth <jinuzilla@gmail.com>
2024-11-15 17:38:48 -0500

    authgurd added

--- assignment3/jobsphere/src/auth/jwt-auth.guard.ts
+++ assignment3/jobsphere/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    // Here, you can attach more properties to the request.user object
+    return { userId: payload.sub, username: payload.username };
+  }
+}
